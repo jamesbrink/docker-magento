f0e46c294bf63b576b131871b856c4fafac71fc5
James Brink <brink.james@gmail.com>
2018-03-20 22:06:21 -0700

    Updates to fix issues with deploying sample data

--- 2.1.12/docker-assets/usr/local/bin/entrypoint.sh
+++ 2.1.12/docker-assets/usr/local/bin/entrypoint.sh
@@ -11,6 +11,28 @@ install_magento ()
     --currency=USD --timezone=America/Chicago --use-rewrites=1 \
     --backend-frontname=$MAGENTO_ADMINURI
 }
+
+toggle_pagespeed ()
+{
+  if [[ "$MOD_PAGESPEED_ENABLED" == "true" ]]; then
+    echo "Enabling mod_pagespeed"
+    a2enmod pagespeed
+  else
+    echo "Disabling mod_pagespeed"
+    a2dismod pagespeed
+  fi
+}
+
+set_magento_mode ()
+{
+  if ! $(magento deploy:mode:show | grep -iq $MAGENTO_MODE); then
+    echo "Setting Magento mode: $MAGENTO_MODE"
+    magento deploy:mode:set $MAGENTO_MODE
+  else
+    echo "Magento mode already $MAGENTO_MODE."
+  fi
+}
+
 echo "========Magento Settings========"
 echo "Magento Mode: $MAGENTO_MODE"
 echo "Magento Host: $MAGENTO_HOST"
@@ -21,6 +43,15 @@ echo "Magento Admin URI: $MAGENTO_ADMINURI"
 echo "Database Host: $MARIADB_HOST"
 echo "Database User: $MAGENTO_DATABASE_USER"
 echo "Database Password: $MAGENTO_DATABASE_PASSWORD"
+
+# Install Magento sample data.
+if [[ "$ENABLE_SAMPLE_DATA" == "true" ]]; then
+  echo "Enabling Magento sample data."
+  php -f /var/www/magento2-sample-data/dev/tools/build-sample-data.php -- --ce-source=/var/www/html/
+else
+  echo "Disabling Magento sample data."
+  php -f /var/www/magento2-sample-data/dev/tools/build-sample-data.php – --command=unlink --ce-source=/var/www/html/
+fi
 
 # Install Magento on first run.
 if [[ ! -e /var/www/html/app/etc/env.php ]]; then
@@ -33,43 +64,35 @@ if [[ ! -e /var/www/html/app/etc/env.php ]]; then
   done
   # Secure permisions
   chmod 500 /var/www/html/app/etc
-  # Set Magento mode
-  magento deploy:mode:set $MAGENTO_MODE
 else
   echo "Magento is already installed."
 fi
 
-# Toggle mod_pagespeed.
-if [[ "$MOD_PAGESPEED_ENABLED" == "true" ]]; then
-  echo "Enabling mod_pagespeed"
-  a2enmod pagespeed
-else
-  echo "Disabling mod_pagespeed"
-  a2dismod pagespeed
-fi
-
-# Install Magento sample data.
-if [[ "$ENABLE_SAMPLE_DATA" == "true" ]]; then
-  echo "Enabling Magento sample data."
-  php -f /var/www/magento2-sample-data/dev/tools/build-sample-data.php -- --ce-source=/var/www/html/
-else
-  echo "Disabling Magento sample data."
-  php -f /var/www/magento2-sample-data/dev/tools/build-sample-data.php – --command=unlink --ce-source=/var/www/html/
-fi
-
-# Run the cron job every 1 minute
-while :;do magento cron:run | grep -v "Ran jobs by schedule";sleep 60; done &
-
 # Compile SASS themes.
-echo "Compiling SASS"
-cd /var/www/html/vendor/snowdog/frontools
-npx gulp styles
-cd /var/www/html/
+if [[ "$MAGENTO_MODE" == "developer" && -e /var/www/html/vendor/snowdog/frontools/package.json ]]; then
+  echo "Starting SASS backround task."
+  cd /var/www/html/vendor/snowdog/frontools
+  npx gulp watch &
+  gulp_pid="$!"
+  trap "echo 'Stopping SASS background task - pid: $gulp_pid'; kill -SIGTERM $gulp_pid" SIGINT SIGTERM
+  cd /var/www/html
+elif [[ -e /var/www/html/vendor/snowdog/frontools/package.json ]]; then
+  echo "Compiling SASS"
+  cd /var/www/html/vendor/snowdog/frontools
+  npx gulp styles
+  cd /var/www/html/
+fi
+
+set_magento_mode
 
 # Start apache
+toggle_pagespeed
 apache2-foreground &
 pid="$!"
 trap "echo 'Stopping Magento - pid: $pid'; kill -SIGTERM $pid" SIGINT SIGTERM
+
+# Run the cron job every 1 minute
+while :;do magento cron:run | grep -v "Ran jobs by schedule";sleep 60; done &
 
 # Wait for process to end.
 while kill -0 $pid > /dev/null 2>&1; do
--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 #!/usr/bin/make -f
 NAME=jamesbrink/magento
 TEMPLATE=Dockerfile.template
+DOCKER_COMPOSE_TEMPLATE=docker-compose.template
 SED:=$(shell [[ `command -v gsed` ]] && echo gsed || echo sed)
 .PHONY: all 2.2.3 2.2.3-sass 2.1.12 2.0.18
 .DEFAULT_GOAL := 2.2.3
@@ -10,6 +11,7 @@ all: 2.2.3 2.2.3-sass 2.1.12 2.0.18
 2.2.3:
 	mkdir -p $(@)
 	printf "`cat $(TEMPLATE)`" $(@) 7.1 $(@) $(@) false > $(@)/Dockerfile
+	printf "`cat $(DOCKER_COMPOSE_TEMPLATE)`" $(@) > $(@)/docker-compose.yml
 	cp -r docker-assets $(@)
 	docker build -t $(NAME):$(@) --build-arg magento_version=$(@) $(@)
 	docker tag $(NAME):$(@) latest
@@ -17,17 +19,20 @@ all: 2.2.3 2.2.3-sass 2.1.12 2.0.18
 2.2.3-sass:
 	mkdir -p $(@)
 	printf "`cat $(TEMPLATE)`" $(@) 7.1 $(@) 2.2.3 true > $(@)/Dockerfile
+	printf "`cat $(DOCKER_COMPOSE_TEMPLATE)`" $(@) > $(@)/docker-compose.yml
 	cp -r docker-assets $(@)
 	docker build -t $(NAME):$(@) --build-arg magento_version=2.2.3 --build-arg sass_enabled=true $(@)
 
 2.1.12:
 	mkdir -p $(@)
 	printf "`cat $(TEMPLATE)`" $(@) 7.0 $(@) $(@) false > $(@)/Dockerfile
+	printf "`cat $(DOCKER_COMPOSE_TEMPLATE)`" $(@) > $(@)/docker-compose.yml
 	cp -r docker-assets $(@)
 	docker build -t $(NAME):$(@) --build-arg magento_version=$(@) $(@)
 
 2.0.18:
 	mkdir -p $(@)
 	printf "`cat $(TEMPLATE)`" $(@) 7.0 $(@) $(@) false > $(@)/Dockerfile
+	printf "`cat $(DOCKER_COMPOSE_TEMPLATE)`" $(@) > $(@)/docker-compose.yml
 	cp -r docker-assets $(@)
 	docker build -t $(NAME):$(@) --build-arg magento_version=$(@) $(@)
