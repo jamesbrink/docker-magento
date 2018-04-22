3a7a3cf65b2197bfd25879bd7d383a128f7af042
James Brink <brink.james@gmail.com>
2018-04-22 00:37:49 -0700

    Updates to httpd settings as well as removing sample-data from launch script

--- 2.2/docker-assets/usr/local/bin/entrypoint.sh
+++ 2.2/docker-assets/usr/local/bin/entrypoint.sh
@@ -1,77 +1,99 @@
-#!/bin/sh
+#!/bin/bash
 
-cd /magento/htdocs
+# Used for simple logging purposes.
+timestamp="date +\"%Y-%m-%d %H:%M:%S\""
+alias echo="echo \"$(eval $timestamp) -$@\""
 
-install_magento ()
-{
-  magento setup:install --base-url=http://$MAGENTO_HOST/ \
-    --db-host=$MARIADB_HOST --db-name=$MAGENTO_DATABASE_NAME \
-    --db-user=$MAGENTO_DATABASE_USER --db-password=$MAGENTO_DATABASE_PASSWORD \
-    --admin-firstname=$MAGENTO_FIRSTNAME --admin-lastname=$MAGENTO_LASTNAME \
-    --admin-email=$MAGENTO_EMAIL --admin-user=$MAGENTO_USERNAME \
-    --admin-password=$MAGENTO_PASSWORD --language=en_US \
-    --currency=USD --timezone=America/Chicago --use-rewrites=1 \
-    --backend-frontname=$MAGENTO_ADMINURI
+cd $MAGENTO_HOME
+
+install_magento() {
+	magento setup:install --base-url=http://$MAGENTO_HOST/ \
+		--db-host=$MARIADB_HOST --db-name=$MAGENTO_DATABASE_NAME \
+		--db-user=$MAGENTO_DATABASE_USER --db-password=$MAGENTO_DATABASE_PASSWORD \
+		--admin-firstname=$MAGENTO_FIRSTNAME --admin-lastname=$MAGENTO_LASTNAME \
+		--admin-email=$MAGENTO_EMAIL --admin-user=$MAGENTO_USERNAME \
+		--admin-password=$MAGENTO_PASSWORD --language=en_US \
+		--currency=USD --timezone=America/Chicago --use-rewrites=1 \
+		--backend-frontname=$MAGENTO_ADMINURI
 }
 
-toggle_pagespeed ()
-{
-  if [[ "$MOD_PAGESPEED_ENABLED" == "true" ]]; then
-    echo "Enabling mod_pagespeed"
-    a2enmod pagespeed
-  else
-    echo "Disabling mod_pagespeed"
-    a2dismod pagespeed
-  fi
+toggle_pagespeed() {
+	if [[ "$MOD_PAGESPEED_ENABLED" == "true" ]]; then
+		echo "Enabling mod_pagespeed"
+		a2enmod pagespeed
+	else
+		echo "Disabling mod_pagespeed"
+		a2dismod pagespeed
+	fi
 }
 
-set_magento_mode ()
-{
-  if ! $(magento deploy:mode:show | grep -iq $MAGENTO_MODE); then
-    echo "Setting Magento mode: $MAGENTO_MODE"
-    magento deploy:mode:set $MAGENTO_MODE
-  else
-    echo "Magento mode already $MAGENTO_MODE."
-  fi
+set_magento_mode() {
+	if ! $(magento deploy:mode:show | grep -iq $MAGENTO_MODE); then
+		echo "Setting Magento mode: $MAGENTO_MODE"
+		magento deploy:mode:set $MAGENTO_MODE
+		
+		# This makes a massive difference in response times.
+		magento setup:di:compile
+	else
+		echo "Magento mode already $MAGENTO_MODE."
+	fi
 }
 
-enabled_xdebug () {
-  echo "zend_extension=$(find /usr/local/lib/php/extensions/ -name xdebug.so)" > /usr/local/etc/php/conf.d/xdebug.ini
-  echo "xdebug.remote_enable=on" >> /usr/local/etc/php/conf.d/xdebug.ini
-  echo "xdebug.remote_autostart=off" >> /usr/local/etc/php/conf.d/xdebug.ini
+enabled_xdebug() {
+	echo "zend_extension=$(find /usr/local/lib/php/extensions/ -name xdebug.so)" >/usr/local/etc/php/conf.d/xdebug.ini
+	echo "xdebug.remote_enable=on" >>/usr/local/etc/php/conf.d/xdebug.ini
+	echo "xdebug.remote_autostart=off" >>/usr/local/etc/php/conf.d/xdebug.ini
 }
 
-compile_sass () {
-  if [[ "$MAGENTO_MODE" == "developer" && -e /var/www/html/vendor/snowdog/frontools/package.json ]]; then
-    echo "Starting SASS backround task."
-    cd /var/www/html/vendor/snowdog/frontools
-    npx gulp watch &
-    gulp_pid="$!"
-    trap "echo 'Stopping SASS background task - pid: $gulp_pid'; kill -SIGTERM $gulp_pid" SIGINT SIGTERM
-    cd /var/www/html
-  elif [[ -e /var/www/html/vendor/snowdog/frontools/package.json ]]; then
-    echo "Compiling SASS"
-    cd /var/www/html/vendor/snowdog/frontools
-    npx gulp styles &
-    cd /var/www/html/
-  fi
+compile_sass() {
+	if [[ "$MAGENTO_MODE" == "developer" && -e /var/www/html/vendor/snowdog/frontools/package.json ]]; then
+		echo "Starting SASS backround task."
+		cd $MAGENTO_HOME/vendor/snowdog/frontools
+		npx gulp watch &
+		gulp_pid="$!"
+		trap "echo 'Stopping SASS background task - pid: $gulp_pid'; kill -SIGTERM $gulp_pid" SIGINT SIGTERM
+		cd -
+	elif [[ -e /var/www/html/vendor/snowdog/frontools/package.json ]]; then
+		echo "Compiling SASS"
+		cd $MAGENTO_HOME/vendor/snowdog/frontools
+		npx gulp styles --prod &
+		cd -
+	fi
 }
 
-install_sample_data () {
-  echo "Installing Magento Sample Data"
-  cd /magento/
-  curl -L -O https://github.com/magento/magento2-sample-data/archive/${MAGENTO_VERSION}.tar.gz
-  tar xfvz ${MAGENTO_VERSION}.tar.gz
-  rm ${MAGENTO_VERSION}.tar.gz
-  mv magento2-sample-data-${MAGENTO_VERSION} sample-data
-  php -f /magento/sample-data/dev/tools/build-sample-data.php -- --ce-source=/magento/htdocs/
-}
-
-uninstall_sample_data () {
-  echo "Removing Magento Sample Data"
-  cd /magento/
-  php -f /magento/sample-data/dev/tools/build-sample-data.php – --command=unlink --ce-source=/magento/htdocs/
-  rm -rf sample-data
+enable_redis() {
+	mv $MAGENTO_HOME/app/etc/env.php $MAGENTO_HOME/app/etc/env.php.orig
+	echo "Enabling Redis Cache"
+	head -n -1 $MAGENTO_HOME/app/etc/env.php.orig > $MAGENTO_HOME/app/etc/env.php
+	cat <<-EOF >>$MAGENTO_HOME/app/etc/env.php
+		'cache' =>
+		array(
+		'frontend' =>
+		array(
+			'default' =>
+			array(
+				'backend' => 'Cm_Cache_Backend_Redis',
+				'backend_options' =>
+				array(
+					'server' => '$REDIS_HOST',
+					'database' => '0',
+					'port' => '$REDIS_PORT'
+					),
+			),
+			'page_cache' =>
+			array(
+			'backend' => 'Cm_Cache_Backend_Redis',
+			'backend_options' =>
+			array(
+				'server' => '$REDIS_HOST',
+				'port' => '$REDIS_PORT',
+				'database' => '1',
+				'compress_data' => '0'
+			)
+			)
+		)
+		),);
+	EOF
 }
 
 echo "========Magento Settings========"
@@ -85,45 +107,44 @@ echo "Database Host: $MARIADB_HOST"
 echo "Database User: $MAGENTO_DATABASE_USER"
 echo "Database Password: $MAGENTO_DATABASE_PASSWORD"
 
-
-# Toggle Magento Sample Data
-if [ "$ENABLE_SAMPLE_DATA" = "true" ]; then
-  install_sample_data
-elif [ -e /magento/sample_data ]; then
-  uninstall_sample_data
-fi
-
-# Install Magento on first run.
-if [ ! -e /magento/htdocs/app/etc/env.php ]; then
-  echo "========Installing Magento========"
-  until install_magento
-  do
-    # TODO replace this, this is a very naive approach.
-    echo "Waiting for MariaDB"
-    sleep 10
-  done
-  # Secure permisions
-  echo "running chmod 500 on /magento/app/etc"
-  chmod 500 /magento/htdocs/app/etc
-else
-  echo "Magento is already installed."
-fi
-
-compile_sass
-set_magento_mode
-
 # Start apache
-# toggle_pagespeed
 echo "Starting Apache"
 httpd -D FOREGROUND &
 pid="$!"
 trap "echo 'Stopping Magento - pid: $pid'; kill -SIGTERM $pid" SIGINT SIGTERM
 
+# Install Magento on first run.
+if [ ! -e $MAGENTO_HOME/app/etc/env.php ]; then
+	echo "========Installing Magento========"
+	until install_magento; do
+		# TODO replace this, this is a very naive approach.
+		echo "Waiting for MariaDB"
+		sleep 10
+	done
+	
+	# Enable Redis for cache if redis hostname is supplied.
+	if [ "$REDIS_HOST" != "" ]; then
+		enable_redis
+	fi
+
+	# Secure permisions
+	echo "running chmod 500 on /magento/app/etc"
+	chmod 500 $MAGENTO_HOME/app/etc
+else
+	echo "Magento is already installed."
+fi
+
+compile_sass
+set_magento_mode
+
 # Run the cron job every 1 minute
-while :;do magento cron:run | grep -v "Ran jobs by schedule";sleep 60; done &
+while :; do
+	magento cron:run | grep -v "Ran jobs by schedule"
+	sleep 60
+done &
 
 # Wait for process to end.
-while kill -0 $pid > /dev/null 2>&1; do
-    wait
+while kill -0 $pid >/dev/null 2>&1; do
+	wait
 done
 echo "Exiting"
--- Makefile
+++ Makefile
@@ -2,25 +2,47 @@
 NAME=jamesbrink/magento
 TEMPLATE=Dockerfile.template
 DOCKER_COMPOSE_TEMPLATE=docker-compose.template
-SED:=$(shell [[ `command -v gsed` ]] && echo gsed || echo sed)
-.PHONY: all clean 2.2 2.2-sass 2.1 2.0
+
+.PHONY: all clean 2.2 2.2-sample-data 2.2-sass 2.2-sass-sample-data 2.1 2.0
 .DEFAULT_GOAL := 2.2
 
-all: 2.2 2.2-sass 2.1 2.0
+all: 2.2 2.2-sample-data 2.2-sass 2.2-sass-sample-data 2.1 2.0
 
 2.2:
 	mkdir -p $(@)
 	printf "`cat $(TEMPLATE)`" 2.2 7.1 2.2.3 false 2.2.3 > $(@)/Dockerfile
 	printf "`cat $(DOCKER_COMPOSE_TEMPLATE)`" $(@) > $(@)/docker-compose.yml
+	cp .dockerignore $(@)/.dockerignore
 	cp -rp docker-assets $(@)
 	cp -rp hooks $(@)
 	docker build -t $(NAME):$(@) $(@)
 	docker tag $(NAME):$(@) latest
 
+2.2-sample-data:
+	mkdir -p $(@)
+	printf "`cat $(TEMPLATE)`" 2.2 7.1 2.2.3 false 2.2.3 > $(@)/Dockerfile
+	sed -i -r 's/ARG ENABLE_SAMPLE_DATA.*/ARG ENABLE_SAMPLE_DATA="true"/g' $(@)/Dockerfile
+	printf "`cat $(DOCKER_COMPOSE_TEMPLATE)`" $(@) > $(@)/docker-compose.yml
+	cp .dockerignore $(@)/.dockerignore
+	cp -rp docker-assets $(@)
+	cp -rp hooks $(@)
+	docker build --build-arg ENABLE_SAMPLE_DATA="true" -t $(NAME):$(@) $(@)
+
 2.2-sass:
 	mkdir -p $(@)
 	printf "`cat $(TEMPLATE)`" 2.2 7.1 2.2.3 true 2.2.3 > $(@)/Dockerfile
 	printf "`cat $(DOCKER_COMPOSE_TEMPLATE)`" $(@) > $(@)/docker-compose.yml
+	cp .dockerignore $(@)/.dockerignore
+	cp -rp docker-assets $(@)
+	cp -rp hooks $(@)
+	docker build -t $(NAME):$(@) $(@)
+
+2.2-sass-sample-data:
+	mkdir -p $(@)
+	printf "`cat $(TEMPLATE)`" 2.2 7.1 2.2.3 true 2.2.3 > $(@)/Dockerfile
+	sed -i -r 's/ARG ENABLE_SAMPLE_DATA.*/ARG ENABLE_SAMPLE_DATA="true"/g' $(@)/Dockerfile
+	printf "`cat $(DOCKER_COMPOSE_TEMPLATE)`" $(@) > $(@)/docker-compose.yml
+	cp .dockerignore $(@)/.dockerignore
 	cp -rp docker-assets $(@)
 	cp -rp hooks $(@)
 	docker build -t $(NAME):$(@) $(@)
@@ -29,6 +51,7 @@ all: 2.2 2.2-sass 2.1 2.0
 	mkdir -p $(@)
 	printf "`cat $(TEMPLATE)`" 2.2 7.0 2.2.3 false 2.2.3 > $(@)/Dockerfile
 	printf "`cat $(DOCKER_COMPOSE_TEMPLATE)`" $(@) > $(@)/docker-compose.yml
+	cp .dockerignore $(@)/.dockerignore
 	cp -rp docker-assets $(@)
 	cp -rp hooks $(@)
 	docker build -t $(NAME):$(@) $(@)
@@ -37,9 +60,10 @@ all: 2.2 2.2-sass 2.1 2.0
 	mkdir -p $(@)
 	printf "`cat $(TEMPLATE)`" 2.2 7.0 2.2.3 false 2.2.3 > $(@)/Dockerfile
 	printf "`cat $(DOCKER_COMPOSE_TEMPLATE)`" $(@) > $(@)/docker-compose.yml
+	cp .dockerignore $(@)/.dockerignore
 	cp -rp docker-assets $(@)
 	cp -rp hooks $(@)
 	docker build -t $(NAME):$(@) $(@)
 
 clean:
-	rm -rf 2.2 2.2-sass 2.1 2.0
+	rm -rf 2.2 2.2-sample-data 2.2-sass 2.2-sass-sample-data 2.1 2.0
